b42bffdf3cfaed3b883ee8f89143f981b09ef9ed
havancuong2003 <havancuong07122003@gmail.com>
2024-06-12 12:53:12 +0700

    add: add daily

--- src/pages/DailyPlanning/DailyPlanning.js
+++ src/pages/DailyPlanning/DailyPlanning.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./daily.css"; // import file CSS của bạn
+import SideBar from "./SideBar";
+import Main from "./Main";
 
 const DailyPlanning = () => {
-    return <div></div>;
+    return (
+        <Container className="App" fluid>
+            <Row>
+                <Col md={3} className="sidebar">
+                    <SideBar />
+                </Col>
+                <Col
+                    md={9}
+                    className="main"
+                    style={{ borderLeft: "1px solid #ccc" }}
+                >
+                    <Main />
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default DailyPlanning;
