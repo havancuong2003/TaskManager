eeb6608a46c943f8037b73650cf307dadecbdb8c
havancuong2003 <havancuong07122003@gmail.com>
2024-07-02 14:28:54 +0700

    fix: fix daily planning

--- src/pages/DailyPlanning/DailyPlanning.js
+++ src/pages/DailyPlanning/DailyPlanning.js
@@ -7,13 +7,13 @@ import Main from "./Main";
 const DailyPlanning = () => {
     return (
         <Container className="App" fluid style={{ display: "flex" }}>
-            <div style={{ flex: "0 0 15%", maxWidth: "15%" }}>
+            {/* <div style={{ flex: "0 0 15%", maxWidth: "15%" }}>
                 <SideBar />
-            </div>
+            </div> */}
             <div
                 style={{
-                    flex: "0 0 85%",
-                    maxWidth: "85%",
+                    flex: "0 0 100%",
+                    maxWidth: "100%",
                     borderLeft: "1px solid #ccc",
                 }}
             >
