dc015bcd00af906964a0b0eab3388cd3307c02d3
minhbro3003 <minhtiencool03@gmail.com>
2024-07-03 22:33:21 +0700

    update TaskManager

--- src/pages/TaskManagement/Main.js
+++ src/pages/TaskManagement/Main.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Container, Table, Col, Row } from "react-bootstrap";
+import { Table, Col } from "react-bootstrap";
 
 const Main = () => {
     return (
-        <Col sm={8} md={9} lg={10} className="">
+        <Col>
             <h1>Workout Schedule</h1>
+            <p className="button1">
+                <button className="btn">
+                    <i className="bi bi-calendar-plus-fill"></i>
+                </button>
+            </p>
             <Table striped bordered hover>
                 <thead>
                     <tr>
                         <th className="text-center">
+                            <input type="date" className="form-control" />
                             <input type="date" className="form-control" />
                         </th>
                         <th className="text-center">Sunday</th>
@@ -18,10 +24,41 @@ const Main = () => {
                         <th className="text-center">Thursday</th>
                         <th className="text-center">Friday</th>
                         <th className="text-center">Saturday</th>
-                        <th className="text-center">Action</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td className="text-center">8 am</td>
+                        <td>
+                            <div className="workout-item">
+                                Weight Loss
+                                <br />8 am - 9 am
+                            </div>
+                        </td>
+                        <td></td>
+                        <td>
+                            <div className="workout-item">
+                                Spinning
+                                <br />9 am - 9.30 am
+                            </div>
+                        </td>
+                        <td>
+                            <div className="workout-item">
+                                Body Building
+                                <br />
+                                9.30 am - 10 pm
+                            </div>
+                        </td>
+                        <td></td>
+                        <td>
+                            <div className="workout-item">
+                                Racing
+                                <br />
+                                9.30 am - 10 am
+                            </div>
+                        </td>
+                        <td></td>
+                    </tr>
                     <tr>
                         <td className="text-center">10 am</td>
                         <td>
@@ -55,10 +92,6 @@ const Main = () => {
                             </div>
                         </td>
                         <td></td>
-                        <td>
-                            <i className="bi bi-pencil-square"></i> {"    "}
-                            <i className="bi bi-trash3-fill"></i>
-                        </td>
                     </tr>
                     <tr>
                         <td className="text-center">11 am</td>
@@ -81,10 +114,6 @@ const Main = () => {
                         </td>
                         <td></td>
                         <td></td>
-                        <td>
-                            <i className="bi bi-pencil-square"></i> {"    "}
-                            <i className="bi bi-trash3-fill"></i>
-                        </td>
                     </tr>
                     <tr>
                         <td className="text-center">03 pm</td>
@@ -125,10 +154,6 @@ const Main = () => {
                                 03 pm - 04 pm
                             </div>
                         </td>
-                        <td>
-                            <i className="bi bi-pencil-square"></i> {"    "}
-                            <i className="bi bi-trash3-fill"></i>
-                        </td>
                     </tr>
                     <tr>
                         <td className="text-center">05 pm</td>
@@ -162,10 +187,6 @@ const Main = () => {
                                 <br />
                                 05 pm - 06 pm
                             </div>
-                        </td>
-                        <td>
-                            <i className="bi bi-pencil-square"></i> {"    "}
-                            <i className="bi bi-trash3-fill"></i>
                         </td>
                     </tr>
                     <tr>
@@ -201,10 +222,62 @@ const Main = () => {
                             </div>
                         </td>
                         <td></td>
+                    </tr>
+                    <tr>
+                        <td className="text-center">09 pm</td>
+                        <td></td>
                         <td>
-                            <i className="bi bi-pencil-square"></i> {"    "}
-                            <i className="bi bi-trash3-fill"></i>
+                            <div className="workout-item">
+                                Crossfit
+                                <br />
+                                07 pm - 08 pm
+                            </div>
                         </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <div className="workout-item">
+                                Health
+                                <br />
+                                07 pm - 08 pm
+                            </div>
+                        </td>
+                        <td>
+                            <div className="workout-item">
+                                Jumping
+                                <br />
+                                07 pm - 08 pm
+                            </div>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="text-center">10 pm</td>
+                        <td>
+                            <div className="workout-item">
+                                Health
+                                <br />
+                                07 pm - 08 pm
+                            </div>
+                        </td>
+                        <td></td>
+                        <td>
+                            <div className="workout-item">
+                                Boxercise
+                                <br />
+                                07 pm - 08 pm
+                            </div>
+                        </td>
+                        <td></td>
+                        <td>
+                            <div className="workout-item">
+                                Jumping
+                                <br />
+                                07 pm - 08 pm
+                            </div>
+                        </td>
+                        <td></td>
+                        <td></td>
                     </tr>
                 </tbody>
             </Table>
