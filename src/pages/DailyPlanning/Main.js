6df3daffdcc04f09e41739a250735195c382ccca
havancuong2003 <havancuong07122003@gmail.com>
2024-07-05 14:52:47 +0700

    fix: fix daily

--- src/pages/DailyPlanning/Main.js
+++ src/pages/DailyPlanning/Main.js
@@ -17,6 +17,7 @@ import {
     TextField,
     Button,
     FormGroup,
+    Checkbox,
 } from "@mui/material";
 import { AddCircle, Search } from "@mui/icons-material";
 import { checkTimeConflict } from "../../utils/timeUtil";
@@ -45,7 +46,11 @@ const Main = () => {
                 throw new Error("Network response was not ok");
             }
             const jsonData = await response.json();
-            sortData(jsonData, sortConfig);
+            const currentDate = new Date().toISOString().slice(0, 10);
+            const filteredData = jsonData.filter(
+                (task) => task.date === currentDate
+            );
+            sortData(filteredData, sortConfig);
         } catch (error) {
             console.error("Error fetching data: ", error);
         }
@@ -74,7 +79,15 @@ const Main = () => {
     };
 
     useEffect(() => {
-        fetchData();
+        const interval = setInterval(() => {
+            fetchData();
+        }, 60000); // Cập nhật dữ liệu mỗi 1 phút
+
+        fetchData(); // Fetch lần đầu khi component được mount
+
+        return () => {
+            clearInterval(interval); // Dừng interval khi component unmount
+        };
     }, []);
 
     const handleOpenModal = () => {
@@ -95,6 +108,22 @@ const Main = () => {
 
     const handleAddTask = async () => {
         try {
+            const startDateTime = new Date(
+                `${newTask.date}T${newTask.timeStart}:00`
+            );
+            if (startDateTime < new Date()) {
+                alert("Không thể thêm công việc trong quá khứ.");
+                return;
+            }
+
+            const endDateTime = new Date(
+                `${newTask.date}T${newTask.timeEnd}:00`
+            );
+            if (endDateTime < new Date()) {
+                alert("Không thể thêm công việc trong quá khứ.");
+                return;
+            }
+
             const hasConflict = checkTimeConflict(
                 {
                     ...newTask,
@@ -107,13 +136,16 @@ const Main = () => {
             if (hasConflict) {
                 alert("Thời gian bị trùng lặp với công việc khác.");
             } else {
+                const currentDate = new Date().toISOString().slice(0, 10);
                 const newTaskWithId = {
                     id: uuidv4(),
+                    date: currentDate,
                     start: newTask.timeStart,
                     end: newTask.timeEnd,
                     activity: newTask.activity,
                     location: newTask.location,
                     description: newTask.description,
+                    completed: false,
                 };
 
                 const response = await fetch("http://localhost:9999/schedule", {
@@ -131,7 +163,7 @@ const Main = () => {
                 const addedTask = await response.json();
 
                 setData((prevData) => [...prevData, addedTask]);
-                sortData([...data, addedTask], sortConfig); // Sort data after adding new task
+                sortData([...data, addedTask], sortConfig);
 
                 handleCloseModal();
             }
@@ -140,7 +172,12 @@ const Main = () => {
         }
     };
 
-    const handleDeleteTask = async (id) => {
+    const handleDeleteTask = async (id, disabled) => {
+        if (disabled) {
+            alert("Không thể xóa công việc đã qua thời gian.");
+            return;
+        }
+
         if (window.confirm("Bạn có chắc chắn muốn xóa công việc này?")) {
             try {
                 const response = await fetch(
@@ -174,112 +211,151 @@ const Main = () => {
             task.description.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    const isTaskDisabled = (task) => {
+        const now = new Date();
+        const [hours, minutes] = task.end.split(":");
+        const taskEndTime = new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            hours,
+            minutes
+        );
+
+        return taskEndTime < now;
+    };
+
+    const handleEditTask = (task) => {
+        setNewTask({ ...task });
+        setOpenModal(true);
+    };
+
+    const handleTaskComplete = async (taskId) => {
+        try {
+            const taskToUpdate = data.find((task) => task.id === taskId);
+            if (!taskToUpdate) {
+                throw new Error("Task not found");
+            }
+
+            const updatedTask = {
+                ...taskToUpdate,
+                completed: !taskToUpdate.completed,
+            };
+
+            const response = await fetch(
+                `http://localhost:9999/schedule/${taskId}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(updatedTask),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const updatedTaskFromServer = await response.json();
+
+            setData((prevData) =>
+                prevData.map((task) =>
+                    task.id === updatedTaskFromServer.id
+                        ? updatedTaskFromServer
+                        : task
+                )
+            );
+        } catch (error) {
+            console.error("Error updating task completion: ", error);
+        }
+    };
+
     return (
         <div>
             <AppBar position="static">
-                <Toolbar className="flex justify-between">
-                    <div className="flex items-center">
-                        <Typography variant="h6" noWrap>
-                            Lịch Trình Hàng Ngày ( cần làm thêm theo dõi hàng
-                            ngày xem có thực hiện đủ hay không)
-                        </Typography>
-                        <IconButton color="inherit" onClick={handleOpenModal}>
-                            <AddCircle />
-                        </IconButton>
-                    </div>
-                    <div className="relative mr-2">
-                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <Search />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            className="pl-10 pr-4 py-2 rounded-full bg-gray-100"
-                            inputProps={{ "aria-label": "search" }}
-                            onChange={handleSearch}
-                            value={searchTerm}
-                        />
-                    </div>
+                <Toolbar>
+                    <Typography variant="h6">Daily Task Manager</Typography>
                 </Toolbar>
             </AppBar>
             <Box m={2}>
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
+                <Paper component="form">
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                    <InputBase
+                        placeholder="Search tasks..."
+                        onChange={handleSearch}
+                    />
+                </Paper>
+            </Box>
+            <Box m={2}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<AddCircle />}
+                    onClick={handleOpenModal}
+                >
+                    Add Task
+                </Button>
+            </Box>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell
+                                onClick={() => handleSort("start")}
+                                style={{ cursor: "pointer" }}
+                            >
+                                Time
+                            </TableCell>
+                            <TableCell>Activity</TableCell>
+                            <TableCell>Location</TableCell>
+                            <TableCell>Description</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {filteredData.map((row) => (
+                            <TableRow key={row.id}>
                                 <TableCell>
-                                    <span
-                                        className="cursor-pointer"
-                                        onClick={() => handleSort("start")}
-                                    >
-                                        Thời Gian
-                                    </span>
+                                    {row.start} - {row.end}
                                 </TableCell>
+                                <TableCell>{row.activity}</TableCell>
+                                <TableCell>{row.location}</TableCell>
+                                <TableCell>{row.description}</TableCell>
                                 <TableCell>
-                                    <span
-                                        className="cursor-pointer"
-                                        onClick={() => handleSort("activity")}
+                                    <Checkbox
+                                        disabled={isTaskDisabled(row)}
+                                        checked={row.completed}
+                                        onChange={() =>
+                                            handleTaskComplete(row.id)
+                                        }
+                                    />
+                                    <Button
+                                        color="primary"
+                                        style={{ marginLeft: 8 }}
+                                        onClick={() => handleEditTask(row)} // Pass task object to handleEditTask
                                     >
-                                        Hoạt Động
-                                    </span>
-                                </TableCell>
-                                <TableCell>
-                                    <span
-                                        className="cursor-pointer"
-                                        onClick={() => handleSort("location")}
-                                    >
-                                        Địa Điểm
-                                    </span>
-                                </TableCell>
-                                <TableCell>
-                                    <span
-                                        className="cursor-pointer"
+                                        <i className="bi bi-pencil-square"></i>
+                                    </Button>
+                                    <Button
+                                        color="secondary"
                                         onClick={() =>
-                                            handleSort("description")
+                                            handleDeleteTask(
+                                                row.id,
+                                                isTaskDisabled(row)
+                                            )
                                         }
                                     >
-                                        Mô Tả
-                                    </span>
+                                        <i className="bi bi-trash3-fill"></i>
+                                    </Button>
                                 </TableCell>
-                                <TableCell>Thao Tác</TableCell>
                             </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {filteredData.map((row) => (
-                                <TableRow key={row.id}>
-                                    <TableCell>
-                                        {row.start} - {row.end}
-                                    </TableCell>
-                                    <TableCell>{row.activity}</TableCell>
-                                    <TableCell>{row.location}</TableCell>
-                                    <TableCell>{row.description}</TableCell>
-                                    <TableCell>
-                                        <Button
-                                            color="primary"
-                                            style={{ marginRight: 10 }}
-                                        >
-                                            <i
-                                                className="bi bi-pencil-square"
-                                                title="Edit"
-                                            ></i>
-                                        </Button>
-                                        <Button
-                                            color="secondary"
-                                            onClick={() =>
-                                                handleDeleteTask(row.id)
-                                            }
-                                        >
-                                            <i
-                                                className="bi bi-trash3"
-                                                title="Delete"
-                                            ></i>
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <Modal open={openModal} onClose={handleCloseModal}>
                 <Box
                     sx={{
@@ -293,66 +369,85 @@ const Main = () => {
                         p: 4,
                     }}
                 >
-                    <Typography variant="h6" component="h2">
-                        Thêm Công Việc Mới
+                    <Typography variant="h6" gutterBottom>
+                        Add New Task
                     </Typography>
-                    <FormGroup>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Thời Gian Bắt Đầu"
-                            name="timeStart"
-                            type="time"
-                            value={newTask.timeStart}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Thời Gian Kết Thúc"
-                            name="timeEnd"
-                            type="time"
-                            value={newTask.timeEnd}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Hoạt Động"
-                            name="activity"
-                            value={newTask.activity}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Địa Điểm"
-                            name="location"
-                            value={newTask.location}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Mô Tả"
-                            name="description"
-                            value={newTask.description}
-                            onChange={handleChange}
-                        />
+                    <TextField
+                        fullWidth
+                        type="date"
+                        label="Date"
+                        name="date"
+                        value={newTask.date}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        margin="normal"
+                    />
+                    <TextField
+                        fullWidth
+                        type="time"
+                        label="Start Time"
+                        name="timeStart"
+                        value={newTask.timeStart}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        margin="normal"
+                    />
+                    <TextField
+                        fullWidth
+                        type="time"
+                        label="End Time"
+                        name="timeEnd"
+                        value={newTask.timeEnd}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        margin="normal"
+                    />
+                    <TextField
+                        fullWidth
+                        label="Activity"
+                        name="activity"
+                        value={newTask.activity}
+                        onChange={handleChange}
+                        margin="normal"
+                    />
+                    <TextField
+                        fullWidth
+                        label="Location"
+                        name="location"
+                        value={newTask.location}
+                        onChange={handleChange}
+                        margin="normal"
+                    />
+                    <TextField
+                        fullWidth
+                        label="Description"
+                        name="description"
+                        value={newTask.description}
+                        onChange={handleChange}
+                        margin="normal"
+                    />
+                    <Box mt={2}>
                         <Button
-                            type="submit"
                             variant="contained"
                             color="primary"
                             onClick={handleAddTask}
                         >
-                            Thêm
+                            Add
                         </Button>
-                    </FormGroup>
+                        <Button
+                            variant="contained"
+                            onClick={handleCloseModal}
+                            style={{ marginLeft: "10px" }}
+                        >
+                            Cancel
+                        </Button>
+                    </Box>
                 </Box>
             </Modal>
         </div>
