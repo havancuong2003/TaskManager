48fa304e17c44e8ab6a3d0d69ca56b587bd938cc
havancuong2003 <havancuong07122003@gmail.com>
2024-07-05 22:28:08 +0700

    fix: fix daily

--- src/pages/DailyPlanning/Main.js
+++ src/pages/DailyPlanning/Main.js
@@ -3,8 +3,6 @@ import {
     AppBar,
     Toolbar,
     Typography,
-    IconButton,
-    InputBase,
     Box,
     Table,
     TableBody,
@@ -13,31 +11,22 @@ import {
     TableHead,
     TableRow,
     Paper,
-    Modal,
-    TextField,
-    Button,
-    FormGroup,
     Checkbox,
+    MenuItem,
+    Select,
+    FormControl,
+    InputLabel,
 } from "@mui/material";
-import { AddCircle, Search } from "@mui/icons-material";
-import { checkTimeConflict } from "../../utils/timeUtil";
-import { v4 as uuidv4 } from "uuid";
 
 const Main = () => {
     const [data, setData] = useState([]);
-    const [openModal, setOpenModal] = useState(false);
-    const [newTask, setNewTask] = useState({
-        timeStart: "",
-        timeEnd: "",
-        activity: "",
-        location: "",
-        description: "",
-    });
-    const [sortConfig, setSortConfig] = useState({
-        key: "start",
-        direction: "asc",
-    });
-    const [searchTerm, setSearchTerm] = useState("");
+    const [template, setTemplate] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(
+        new Date().toISOString().slice(0, 10)
+    );
+    const [dates, setDates] = useState([]);
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [tasksCreated, setTasksCreated] = useState(false); // State to track if tasks are created for the current date
 
     const fetchData = async () => {
         try {
@@ -46,188 +35,115 @@ const Main = () => {
                 throw new Error("Network response was not ok");
             }
             const jsonData = await response.json();
-            const currentDate = new Date().toISOString().slice(0, 10);
+            const uniqueDates = [...new Set(jsonData.map((task) => task.date))];
+            setDates(uniqueDates);
             const filteredData = jsonData.filter(
-                (task) => task.date === currentDate
+                (task) => task.date === selectedDate
             );
-            sortData(filteredData, sortConfig);
+            setData(filteredData);
         } catch (error) {
             console.error("Error fetching data: ", error);
         }
     };
 
-    const sortData = (data, config) => {
-        const sortedData = data.sort((a, b) => {
-            if (a[config.key] < b[config.key]) {
-                return config.direction === "asc" ? -1 : 1;
-            }
-            if (a[config.key] > b[config.key]) {
-                return config.direction === "asc" ? 1 : -1;
-            }
-            return 0;
-        });
-        setData(sortedData);
-    };
-
-    const handleSort = (key) => {
-        let direction = "asc";
-        if (sortConfig.key === key && sortConfig.direction === "asc") {
-            direction = "desc";
-        }
-        setSortConfig({ key, direction });
-        sortData(data, { key, direction });
-    };
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            fetchData();
-        }, 60000); // Cập nhật dữ liệu mỗi 1 phút
-
-        fetchData(); // Fetch lần đầu khi component được mount
-
-        return () => {
-            clearInterval(interval); // Dừng interval khi component unmount
-        };
-    }, []);
-
-    const handleOpenModal = () => {
-        setOpenModal(true);
-    };
-
-    const handleCloseModal = () => {
-        setOpenModal(false);
-    };
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNewTask((prevTask) => ({
-            ...prevTask,
-            [name]: value,
-        }));
-    };
-
-    const handleAddTask = async () => {
+    const fetchTemplate = async () => {
         try {
-            const startDateTime = new Date(
-                `${newTask.date}T${newTask.timeStart}:00`
-            );
-            if (startDateTime < new Date()) {
-                alert("Không thể thêm công việc trong quá khứ.");
-                return;
+            const response = await fetch("http://localhost:9999/template");
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
             }
+            const jsonData = await response.json();
+            setTemplate(jsonData);
+        } catch (error) {
+            console.error("Error fetching template: ", error);
+        }
+    };
 
-            const endDateTime = new Date(
-                `${newTask.date}T${newTask.timeEnd}:00`
-            );
-            if (endDateTime < new Date()) {
-                alert("Không thể thêm công việc trong quá khứ.");
-                return;
-            }
+    const createTasksFromTemplate = async () => {
+        const currentDate = new Date().toISOString().slice(0, 10);
+        const tasksForToday = template.map((task) => ({
+            ...task,
+            id: task.id.toString(),
+            date: currentDate,
+            completed: false,
+        }));
 
-            const hasConflict = checkTimeConflict(
-                {
-                    ...newTask,
-                    start: newTask.timeStart,
-                    end: newTask.timeEnd,
-                },
-                data
-            );
-
-            if (hasConflict) {
-                alert("Thời gian bị trùng lặp với công việc khác.");
-            } else {
-                const currentDate = new Date().toISOString().slice(0, 10);
-                const newTaskWithId = {
-                    id: uuidv4(),
-                    date: currentDate,
-                    start: newTask.timeStart,
-                    end: newTask.timeEnd,
-                    activity: newTask.activity,
-                    location: newTask.location,
-                    description: newTask.description,
-                    completed: false,
-                };
-
-                const response = await fetch("http://localhost:9999/schedule", {
+        try {
+            for (let task of tasksForToday) {
+                await fetch("http://localhost:9999/schedule", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify(newTaskWithId),
+                    body: JSON.stringify(task),
                 });
-
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-
-                const addedTask = await response.json();
-
-                setData((prevData) => [...prevData, addedTask]);
-                sortData([...data, addedTask], sortConfig);
-
-                handleCloseModal();
             }
+            setTasksCreated(true); // Mark tasks as created for today
+            fetchData(); // Refresh data after creating tasks
         } catch (error) {
-            console.error("Error adding new task: ", error);
+            console.error("Error creating tasks from template: ", error);
         }
     };
 
-    const handleDeleteTask = async (id, disabled) => {
-        if (disabled) {
-            alert("Không thể xóa công việc đã qua thời gian.");
-            return;
-        }
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
 
-        if (window.confirm("Bạn có chắc chắn muốn xóa công việc này?")) {
+        return () => clearInterval(interval);
+    }, []);
+
+    const isTaskDisabled = (endTime) => {
+        const taskEndTime = new Date(currentTime);
+        const [hours, minutes] = endTime.split(":");
+        taskEndTime.setHours(hours, minutes, 0, 0);
+        return taskEndTime < currentTime;
+    };
+
+    useEffect(() => {
+        fetchTemplate();
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const fetchInitialData = async () => {
             try {
-                const response = await fetch(
-                    `http://localhost:9999/schedule/${id}`,
-                    {
-                        method: "DELETE",
-                    }
-                );
+                const currentDate = new Date().toISOString().slice(0, 10);
 
-                if (!response.ok) {
+                // Check if there are tasks for today in the schedule
+                const response = await fetch(
+                    `http://localhost:9999/schedule?date=${currentDate}`
+                );
+                if (response.ok) {
+                    const jsonData = await response.json();
+
+                    if (jsonData.length > 0) {
+                        // Tasks for today already exist, mark as created
+                        setTasksCreated(true);
+                        fetchData(); // Fetch data for the selected date
+                    } else {
+                        // No tasks for today, create tasks from template
+                        createTasksFromTemplate();
+                    }
+                } else {
                     throw new Error("Network response was not ok");
                 }
-
-                setData((prevData) =>
-                    prevData.filter((task) => task.id !== id)
-                );
             } catch (error) {
-                console.error("Error deleting task: ", error);
+                console.error("Error checking tasks for today: ", error);
             }
-        }
-    };
+        };
 
-    const handleSearch = (e) => {
-        setSearchTerm(e.target.value);
-    };
+        fetchInitialData();
 
-    const filteredData = data.filter(
-        (task) =>
-            task.activity.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            task.location.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            task.description.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+        const interval = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
 
-    const isTaskDisabled = (task) => {
-        const now = new Date();
-        const [hours, minutes] = task.end.split(":");
-        const taskEndTime = new Date(
-            now.getFullYear(),
-            now.getMonth(),
-            now.getDate(),
-            hours,
-            minutes
-        );
+        return () => clearInterval(interval);
+    }, [selectedDate]);
 
-        return taskEndTime < now;
-    };
-
-    const handleEditTask = (task) => {
-        setNewTask({ ...task });
-        setOpenModal(true);
+    const handleDateChange = (event) => {
+        setSelectedDate(event.target.value);
     };
 
     const handleTaskComplete = async (taskId) => {
@@ -279,44 +195,35 @@ const Main = () => {
                 </Toolbar>
             </AppBar>
             <Box m={2}>
-                <Paper component="form">
-                    <IconButton>
-                        <Search />
-                    </IconButton>
-                    <InputBase
-                        placeholder="Search tasks..."
-                        onChange={handleSearch}
-                    />
-                </Paper>
-            </Box>
-            <Box m={2}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<AddCircle />}
-                    onClick={handleOpenModal}
-                >
-                    Add Task
-                </Button>
+                <FormControl fullWidth>
+                    <InputLabel id="date-select-label">Select Date</InputLabel>
+                    <Select
+                        labelId="date-select-label"
+                        value={selectedDate}
+                        label="Select Date"
+                        onChange={handleDateChange}
+                    >
+                        {dates.map((date) => (
+                            <MenuItem key={date} value={date}>
+                                {date}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
             </Box>
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell
-                                onClick={() => handleSort("start")}
-                                style={{ cursor: "pointer" }}
-                            >
-                                Time
-                            </TableCell>
+                            <TableCell>Time</TableCell>
                             <TableCell>Activity</TableCell>
                             <TableCell>Location</TableCell>
                             <TableCell>Description</TableCell>
-                            <TableCell>Actions</TableCell>
+                            <TableCell>Completed</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {filteredData.map((row) => (
+                        {data.map((row) => (
                             <TableRow key={row.id}>
                                 <TableCell>
                                     {row.start} - {row.end}
@@ -326,130 +233,18 @@ const Main = () => {
                                 <TableCell>{row.description}</TableCell>
                                 <TableCell>
                                     <Checkbox
-                                        disabled={isTaskDisabled(row)}
                                         checked={row.completed}
                                         onChange={() =>
                                             handleTaskComplete(row.id)
                                         }
+                                        disabled={isTaskDisabled(row.end)} // Disable checkbox if task end time is in the past
                                     />
-                                    <Button
-                                        color="primary"
-                                        style={{ marginLeft: 8 }}
-                                        onClick={() => handleEditTask(row)} // Pass task object to handleEditTask
-                                    >
-                                        <i className="bi bi-pencil-square"></i>
-                                    </Button>
-                                    <Button
-                                        color="secondary"
-                                        onClick={() =>
-                                            handleDeleteTask(
-                                                row.id,
-                                                isTaskDisabled(row)
-                                            )
-                                        }
-                                    >
-                                        <i className="bi bi-trash3-fill"></i>
-                                    </Button>
                                 </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Modal open={openModal} onClose={handleCloseModal}>
-                <Box
-                    sx={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        width: 400,
-                        bgcolor: "background.paper",
-                        boxShadow: 24,
-                        p: 4,
-                    }}
-                >
-                    <Typography variant="h6" gutterBottom>
-                        Add New Task
-                    </Typography>
-                    <TextField
-                        fullWidth
-                        type="date"
-                        label="Date"
-                        name="date"
-                        value={newTask.date}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                    />
-                    <TextField
-                        fullWidth
-                        type="time"
-                        label="Start Time"
-                        name="timeStart"
-                        value={newTask.timeStart}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                    />
-                    <TextField
-                        fullWidth
-                        type="time"
-                        label="End Time"
-                        name="timeEnd"
-                        value={newTask.timeEnd}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                    />
-                    <TextField
-                        fullWidth
-                        label="Activity"
-                        name="activity"
-                        value={newTask.activity}
-                        onChange={handleChange}
-                        margin="normal"
-                    />
-                    <TextField
-                        fullWidth
-                        label="Location"
-                        name="location"
-                        value={newTask.location}
-                        onChange={handleChange}
-                        margin="normal"
-                    />
-                    <TextField
-                        fullWidth
-                        label="Description"
-                        name="description"
-                        value={newTask.description}
-                        onChange={handleChange}
-                        margin="normal"
-                    />
-                    <Box mt={2}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={handleAddTask}
-                        >
-                            Add
-                        </Button>
-                        <Button
-                            variant="contained"
-                            onClick={handleCloseModal}
-                            style={{ marginLeft: "10px" }}
-                        >
-                            Cancel
-                        </Button>
-                    </Box>
-                </Box>
-            </Modal>
         </div>
     );
 };
